48b65c7ebb6baf6126f984f1757f1f6a07ec8686
kimball sumney <kimball.sumney@gmail.com>
2023-07-04 00:36:51 -0500

    added cors

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
 const compression = require('compression');
+const cors = require('cors');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -28,6 +29,9 @@ app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
 // MIDDLEWARE
+app.use(cors());
+
+app.options('*', cors());
 
 // static file location
 app.use(express.static(path.join(__dirname, 'public')));
